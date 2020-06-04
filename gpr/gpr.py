e1e28b5d265c9d7182bb8c6b0b8696bfd196665f
Ilija Medan <imedan@Ilijas-MacBook-Air.local>
2020-06-04 15:52:19 -0400

    Fixed erd columns to gpr.py and demo to match the manuscript

--- gpr/gpr.py
+++ gpr/gpr.py
@@ -32,14 +32,14 @@ def estimate_metals_gpr(data,regr_file,ss_cuts_file,regr_type):
     regr = joblib.load(regr_file)
     if regr_type=='M':
         #create the colors and absolute magntiudes needed for the regression and single star cuts
-        data['M_g']=data['g_PS1']+5*np.log10(1e-3*data['PLX'])+5
-        data['M_H_2MASS']=data['H_2MASS']+5*np.log10(1e-3*data['PLX'])+5
-        data['M_W1']=data['W1_ALLWISE']+5*np.log10(1e-3*data['PLX'])+5
-        data['g-y']=data['g_PS1']-data['y_PS1']
-        data['g-W2']=data['g_PS1']-data['W2_ALLWISE']
-        data['r-W2']=data['r_PS1']-data['W2_ALLWISE']
-        data['r-W1']=data['r_PS1']-data['W1_ALLWISE']
-        data['M_r']=data['r_PS1']+5*np.log10(1e-3*data['PLX'])+5
+        data['M_g']=data['gmag_PS1']+5*np.log10(1e-3*data['PLX_GaiaDR2'])+5
+        data['M_H_2MASS']=data['Hmag_2MASS']+5*np.log10(1e-3*data['PLX_GaiaDR2'])+5
+        data['M_W1']=data['W1mag_AllWISE']+5*np.log10(1e-3*data['PLX_GaiaDR2'])+5
+        data['g-y']=data['gmag_PS1']-data['ymag_PS1']
+        data['g-W2']=data['gmag_PS1']-data['W2mag_AllWISE']
+        data['r-W2']=data['rmag_PS1']-data['W2mag_AllWISE']
+        data['r-W1']=data['rmag_PS1']-data['W1mag_AllWISE']
+        data['M_r']=data['rmag_PS1']+5*np.log10(1e-3*data['PLX_GaiaDR2'])+5
 
         #mean and standard deviation of the training subset used to normalzie the data
         y_mean=-0.06339634364228897
@@ -51,13 +51,13 @@ def estimate_metals_gpr(data,regr_file,ss_cuts_file,regr_type):
         ss_mag='M_r'
     elif regr_type=='K':
         #create the colors and absolute magntiudes needed for the regression and single star cuts
-        data['M_z']=data['z_PS1']+5*np.log10(1e-3*data['PLX'])+5
-        data['g-J']=data['g_PS1']-data['J_2MASS']
-        data['z-y']=data['z_PS1']-data['y_PS1']
-        data['y-W2']=data['y_PS1']-data['W2_ALLWISE']
-        data['W1-W2']=data['W1_ALLWISE']-data['W2_ALLWISE']
-        data['M_W2']=data['W2_ALLWISE']+5*np.log10(1e-3*data['PLX'])+5
-        data['g-H']=data['g_PS1']-data['H_2MASS']
+        data['M_z']=data['zmag_PS1']+5*np.log10(1e-3*data['PLX_GaiaDR2'])+5
+        data['g-J']=data['gmag_PS1']-data['Jmag_2MASS']
+        data['z-y']=data['zmag_PS1']-data['ymag_PS1']
+        data['y-W2']=data['ymag_PS1']-data['W2mag_AllWISE']
+        data['W1-W2']=data['W1mag_AllWISE']-data['W2mag_AllWISE']
+        data['M_W2']=data['W2mag_AllWISE']+5*np.log10(1e-3*data['PLX_GaiaDR2'])+5
+        data['g-H']=data['gmag_PS1']-data['Hmag_2MASS']
 
         #mean and standard deviation of the training subset used to normalzie the data
         y_mean=-0.061195682830781147
