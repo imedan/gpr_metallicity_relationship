2c882d289a131c2106b404d167fd68e6994ea009
imedan <ilija.medan@gmail.com>
2021-02-26 18:12:03 -0500

    Add url

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from setuptools import setup
-import os
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
@@ -12,7 +7,7 @@ setup(
     author="Ilija Medan",
     author_email="medan@astro.gsu.edu",
     description="Photometric Metallicity Relationship for K and M Dwarfs",
-    long_description=read('README.md'),
+    url="https://github.com/imedan/gpr_metallicity_relationship",
     license="BSD 3-Clause",
     py_modules=['gpr.gpr']
     # classifiers=[]
